4cc75e94a90eb8b17236fae99e3e530d0dced423
Nguyễn Sinh Lộc <locnguyen1005@gmail.com>
2023-11-23 21:49:30 +0700

    Create an adapter for notifications

---
+++ app/src/main/java/com/example/jobfindernew/NotificationAdapter.java
@@ -0,0 +1,53 @@
+public class NotificationAdapter extends RecyclerView.Adapter<NotificationAdapter.ViewHolder> {
+    private List<Notification> notifications = new ArrayList<>();
+
+    // Constructor để khởi tạo Adapter
+    public NotificationAdapter() {
+    }
+
+    // Phương thức để thiết lập dữ liệu mới cho Adapter
+    public void setNotifications(List<Notification> notifications) {
+        this.notifications = notifications;
+        notifyDataSetChanged();
+    }
+
+    // Phương thức để tạo ViewHolder mới
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_notification, parent, false);
+        return new ViewHolder(view);
+    }
+
+    // Phương thức để gắn dữ liệu vào ViewHolder
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Notification notification = notifications.get(position);
+        holder.bind(notification);
+    }
+
+    // Phương thức để trả về số lượng item trong danh sách
+    @Override
+    public int getItemCount() {
+        return notifications.size();
+    }
+
+    // Lớp ViewHolder để giữ các thành phần giao diện người dùng
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        private final TextView titleTextView;
+        private final TextView createdAtTextView;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            titleTextView = itemView.findViewById(R.id.titleTextView); // Thay R.id.titleTextView bằng ID thích hợp
+            createdAtTextView = itemView.findViewById(R.id.createdAtTextView); // Thay R.id.createdAtTextView bằng ID thích hợp
+        }
+
+        // Phương thức để gắn dữ liệu vào các thành phần giao diện người dùng
+        public void bind(Notification notification) {
+            titleTextView.setText(notification.getMessage());
+            createdAtTextView.setText(notification.getCreatedAt());
+        }
+    }
+}
