1cc2219a509bf0243a087f5df576ae5c0d3e03e1
Nguyễn Sinh Lộc <locnguyen1005@gmail.com>
2023-11-23 21:54:29 +0700

    Update NotificationActivity.java

--- app/src/main/java/com/example/jobfindernew/NotificationActivity.java
+++ app/src/main/java/com/example/jobfindernew/NotificationActivity.java
@@ -10,5 +10,13 @@ public class NotificationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification);
+
+        RecyclerView recyclerView = findViewById(R.id.rv_notification_list);
+        NotificationAdapter notificationAdapter = new NotificationAdapter();
+        recyclerView.setAdapter(notificationAdapter);
+
+        List<Notification> notificationList = notificationDao.getNotificationsByUserId(userId);
+        notificationAdapter.setNotifications(notificationList);
+
     }
 }
