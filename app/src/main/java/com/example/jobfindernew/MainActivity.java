3837e9c7f57edca7d2e0f721e140ad8e3e88355a
Nguyễn Sinh Lộc <locnguyen1005@gmail.com>
2023-11-24 09:36:02 +0700

    Add functions for the buttons on the main screen

--- app/src/main/java/com/example/jobfindernew/MainActivity.java
+++ app/src/main/java/com/example/jobfindernew/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.jobfindernew;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Nút Đăng nhập/Đăng ký
+        Button btnLogin = findViewById(R.id.btn_login);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Chuyển sang LoginActivity
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // Nút Tìm kiếm công việc
+        Button btnSearch = findViewById(R.id.btn_search);
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Chuyển sang SearchActivity
+                Intent intent = new Intent(MainActivity.this, SearchActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // Nút Tạo tin tuyển dụng
+        Button btnPost = findViewById(R.id.btn_post);
+        btnPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Chuyển sang PostActivity
+                Intent intent = new Intent(MainActivity.this, PostActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
